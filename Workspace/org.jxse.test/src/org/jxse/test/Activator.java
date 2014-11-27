e5210f92c6e0fea603d19587d1adbac3529ea7ab
Kees Pieters <info@condast.com>
2014-11-27 13:37:20 +0100

    Adaptede for changes in jxse

--- Workspace/org.jxse.test/src/org/jxse/test/Activator.java
+++ Workspace/org.jxse.test/src/org/jxse/test/Activator.java
@@ -1,7 +1,7 @@
 package org.jxse.test;
 
 import net.jxse.osgi.loader.JxseActivator;
-import net.jxta.platform.WorldPeerGroupFactory;
+import net.jxse.systemtests.colocated.AdHocTcpCommsTest;
 
 import org.osgi.framework.BundleContext;
 
@@ -12,6 +12,7 @@ public class Activator extends JxseActivator {
 	private static BundleContext context;
 	
 	private _100_Starting_And_Stopping_JXTA_Example example;
+	private AdHocTcpCommsTest test;
 
 	static BundleContext getContext() {
 		return context;
@@ -24,9 +25,14 @@ public class Activator extends JxseActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		super.start(bundleContext);
 		Activator.context = bundleContext;
-		example = new _100_Starting_And_Stopping_JXTA_Example();
+		//example = new _100_Starting_And_Stopping_JXTA_Example();
+		test = new AdHocTcpCommsTest();
+		test.createPeers();
+		test.testComms();
+		test.killAlice();
+		test.killBob();
 		try{
-			example.main(null);
+			//example.main(null);
 		}
 		catch( Exception ex ){
 			ex.printStackTrace();
